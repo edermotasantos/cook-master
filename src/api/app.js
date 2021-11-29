a046ed02e96558c0d29156cd858d48b98447ad3a
Eder Santos <3derms@gmail.com>
2021-11-29 15:12:03 -0300

    iniciando o projeto

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser'); 
 
 const app = express();
+const PORT = 3000;
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
 
 module.exports = app;
